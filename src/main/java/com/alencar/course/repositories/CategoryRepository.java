2b029d6b86b5434dea0fde5656022e50d083a319
Matheus Alencar <matheusalencar6942@gmail.com>
2024-12-05 20:26:40 -0300

    criando entidade categoria e sua camadas

---
+++ src/main/java/com/alencar/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.alencar.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alencar.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
